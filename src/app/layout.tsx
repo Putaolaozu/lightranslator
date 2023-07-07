62205665fcd54b4219323dfd56c564ad2468edb4
Putaolaozu <liweixuan599@gmail.com>
2023-07-07 17:25:26 +0800

    change app name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-        <meta name="apple-mobile-web-app-title" content="光翻" />
+        <meta name="apple-mobile-web-app-title" content="光译" />
         <meta name="theme-color" media="(prefers-color-scheme: light)" content="#eaeff0" />
         <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#0A0A0A" />
         <link rel="apple-touch-icon" href="/app-icons/translate-256.png"></link>

489d0575b577e4e55be35d2f6438e2e400f9958b
Putaolaozu <liweixuan599@gmail.com>
2023-07-07 17:11:07 +0800

    update status bar color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <meta name="apple-mobile-web-app-title" content="光翻" />
-        <meta name="theme-color" media="(prefers-color-scheme: light)" content="cyan" />
-        <meta name="theme-color" media="(prefers-color-scheme: dark)" content="black" />
+        <meta name="theme-color" media="(prefers-color-scheme: light)" content="#eaeff0" />
+        <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#0A0A0A" />
         <link rel="apple-touch-icon" href="/app-icons/translate-256.png"></link>
         <link rel="manifest" href="/manifest.json" />
       </head>

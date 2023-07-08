1fa41d7f52b7365d2d3dd039f96230067a517963
Putaolaozu <liweixuan599@gmail.com>
2023-07-08 09:32:12 +0800

    try pwa offline

--- next.config.js
+++ next.config.js
@@ -1,19 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { serverActions: true },
-};
 
 const withPWA = require("next-pwa")({
   dest: "public",
   fallbacks: {
     image: "/assets/*",
-    document: "/_offline", // if you want to fallback to a custom page other than /_offline
+    // document: "/_offline", // if you want to fallback to a custom page other than /_offline
     // font: '/static/font/fallback.woff2',
     // audio: ...,
     // video: ...,
   },
 });
-module.exports = nextConfig;
+
 module.exports = withPWA({
   images: {
     domains: ["lightfan.netlify.app"],
--- public/sw.js
+++ public/sw.js
@@ -69,7 +69,7 @@ if (!self.define) {
 }
 define(['./workbox-8f0e986c'], (function (workbox) { 'use strict';
 
-  importScripts("fallback-development.js");
+  importScripts();
   self.skipWaiting();
   workbox.clientsClaim();
   workbox.registerRoute("/", new workbox.NetworkFirst({
@@ -90,19 +90,11 @@ define(['./workbox-8f0e986c'], (function (workbox) { 'use strict';
         }
         return response;
       }
-    }, {
-      handlerDidError: async ({
-        request
-      }) => self.fallback(request)
     }]
   }), 'GET');
   workbox.registerRoute(/.*/i, new workbox.NetworkOnly({
     "cacheName": "dev",
-    plugins: [{
-      handlerDidError: async ({
-        request
-      }) => self.fallback(request)
-    }]
+    plugins: []
   }), 'GET');
 
 }));

adaa93c0f78220dac717afd74f5fbd262a0deb81
Putaolaozu <liweixuan599@gmail.com>
2023-07-27 12:00:48 +0800

    add SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="zh-cn">
       <head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
